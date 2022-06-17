cdc862f9349fe69cdb994f0e47d3a6c49ba9138c
Muhamad Farid Ridho Rambe <faridridho@192.168.1.5>
2022-06-17 17:30:07 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Content">
+        <div className="App-header">
+          <div className="Body" >
+            <div className="Profile">
+              <div className="Profile-image">
+                <img src={`https://i.ibb.co/3FM58DT/BPK-resized.png`} alt="profile"></img>
+              </div>
+              <div className="Profile-username">
+                <h1>KALIBER</h1>
+                <p>Katalog Layanan Internal Berbagi</p>
+              </div>
+            </div>
+
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/1oFdVK3x_XKwebR3XycifXnnyLEG0_wrs" target="_blank" rel="noopener noreferrer">RKA 2023</a>
+              </div>
+            </div>
+
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/1oXpamr0dpTc-EEdBhfvipQdWwgGRs1qG" target="_blank" rel="noopener noreferrer">Materi Sosial Perpajakan</a>
+              </div>                
+            </div>
+              
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/14EFfu4Ok49znDR3I8LHpzkVF4SNM4ZK9" target="_blank" rel="noopener noreferrer">Laporan Rutin</a>
+              </div>                
+            </div>
+
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/12rl6Ii3UUNEE_lgmYTp---FQENsJm-01" target="_blank" rel="noopener noreferrer">POK</a>
+              </div>                
+            </div>
+            
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/1NNojHb2_s_1vmzUK7lJ9G4aQ80Uy8Rky" target="_blank" rel="noopener noreferrer">Monitoring Perjadin Terlambat</a>
+              </div>                
+            </div>
+
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/1X40X93gtp95Xx1HqvmfBX-NOI2HVWXKz" target="_blank" rel="noopener noreferrer">Peraturan</a>
+              </div>                
+            </div>
+
+            <div className="Link">
+              <div className="Link-container">
+                <a href="https://drive.google.com/drive/folders/1xs0ttVbbtTZTFp9seaPddWSi8g_-BKvl" target="_blank" rel="noopener noreferrer">Template</a>
+              </div>                
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
